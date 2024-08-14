3301c0b4abe9c34634764954e2ad5c8156578df2
erikd234 <erikd234@gmail.com>
2024-08-14 12:46:46 -0700

    Make the hot reload work for long running commands and command that spawn children with graceful shutdown

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io/fs"
@@ -8,17 +9,17 @@ import (
 	"os/exec"
 	"os/signal"
 	"path/filepath"
+	"regexp"
+	"sync"
 	"syscall"
 	"time"
 )
-
-// CURRENT ONLY WORKS IF THE COMMAND YOU ARE RUNNING TERMINATES
-// TODO is to cancel command that dont terminate and manage them somehow
 
 func main() {
 	// Define flags
 	dir := flag.String("dir", "./", "The directory to change to")
 	cmd := flag.String("cmd", "", "The command to run")
+	only := flag.String("only", ".*", "Regex to match file paths")
 	// Parse flags
 	flag.Parse()
 
@@ -33,42 +34,83 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
-	fmt.Println("Command executed successfully.")
+	regex, err := regexp.Compile(*only)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 	// Create a channel to listen for interrupt or terminate signals
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	// Create a channel to stop the goroutine
 	doneChan := make(chan bool)
-	// this needs to be a buffered channel to avoid deadlock (should really be a return not a channel but whatever haha)
-	fileModChan := make(chan time.Time, 100)
-	lastModTime := time.Now()
+
 	ticker := time.NewTicker(1 * time.Second)
-	// Start a goroutine that runs indefinitely
+
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
+		defer wg.Done()
+		ctx, cancel := context.WithCancel(context.Background())
+		defer cancel()
+
+		var cmdWaitGroup sync.WaitGroup
+		var lastCommand *exec.Cmd
+
+		startCommand := func() {
+			if lastCommand != nil {
+				// If there is a previous command, ensure it has been cleaned up
+				cmdWaitGroup.Wait()
+			}
+
+			ctx, cancel = context.WithCancel(context.Background())
+			cmdWaitGroup.Add(1)
+			go func() {
+				defer cmdWaitGroup.Done()
+				defer cancel()
+				lastCommand, err = runCmdWithContext(ctx, *cmd)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+			}()
+		}
+
+		// Start the initial command
+		startCommand()
+		// pause for a few seconds in case new files are edited at start of runup
+		time.Sleep(3 * time.Second)
+		lastModTime := time.Now()
 		for {
 			select {
-			case lastModTime = <-fileModChan:
-				err := runCmd(*cmd)
-				if err != nil {
-					fmt.Println(err)
-					os.Exit(1)
-				}
 			case <-ticker.C:
-				err := notifyChanFilesUpdated(lastModTime, absDir, fileModChan)
+				ifChanged, err := areFiledUpdated(lastModTime, absDir, regex)
 				if err != nil {
 					fmt.Println(err)
 					os.Exit(1)
 				}
+				if !ifChanged {
+					continue
+				}
+
+				// Cancel the previous command if it was running
+				cancel()
+
+				// Start the new command
+				startCommand()
+				// Update the last modification time
+				// and avoid the recursive command creates files loop
+				time.Sleep(3 * time.Second)
+				lastModTime = time.Now()
+
 			case <-doneChan:
-				fmt.Println("Stopping goroutine...")
+				cancel()
+				cmdWaitGroup.Wait()
 				return
 			}
 		}
-	}()
-	// run the cmd the first time
-	err = runCmd(*cmd)
+	}() // run the cmd the first time
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -80,27 +122,60 @@ func main() {
 
 	// Signal the goroutine to stop
 	doneChan <- true
-
+	fmt.Println("Trying to clean up...")
+	wg.Wait()
 	fmt.Println("Program terminated.")
 }
 
-func runCmd(cmd string) error {
-	command := exec.Command("sh", "-c", cmd)
+func runCmdWithContext(ctx context.Context, cmd string) (*exec.Cmd, error) {
+	command := exec.CommandContext(ctx, "sh", "-c", cmd)
+	command.SysProcAttr = &syscall.SysProcAttr{Setpgid: true} // Create a new process group
 	command.Stdout = os.Stdout
 	command.Stderr = os.Stderr
-	if err := command.Run(); err != nil {
-		fmt.Printf("Failed to execute command %s: %v\n", cmd, err)
-		return err
+	fmt.Printf("Trying... %s\n", cmd)
+	if err := command.Start(); err != nil {
+		fmt.Printf("Failed to start command %s: %v\n", cmd, err)
+		return nil, err
 	}
-	return nil
+
+	cmdDone := make(chan error)
+
+	go func() {
+		cmdDone <- command.Wait()
+	}()
+
+	select {
+	case <-ctx.Done():
+		// Cancel the process and its subprocesses
+		// -pid means send to a whole group
+		if err := syscall.Kill(-command.Process.Pid, syscall.SIGKILL); err != nil {
+			fmt.Printf("Failed to kill process group %d: %v\n", command.Process.Pid, err)
+			return nil, err
+		}
+		fmt.Printf("Process group %d killed\n", command.Process.Pid)
+		return command, nil
+	case err := <-cmdDone:
+		if err != nil {
+			fmt.Printf("Command failed: %v\n", err)
+			return nil, err
+		}
+		fmt.Printf("Command ran successfully %s\n", cmd)
+		return command, nil
+	}
 }
 
 // sends files change signal to a channel
 // I dont need a channel but lets do it anyway
-func notifyChanFilesUpdated(lastModTime time.Time, dir string, ch chan time.Time) error {
+// return true when files update
+func areFiledUpdated(lastModTime time.Time, dir string, regex *regexp.Regexp) (bool, error) {
+	isModded := false
 	err := filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
 		if d.IsDir() {
 			return nil
+		}
+		// Match the file path against the regex
+		if !regex.MatchString(path) {
+			return nil // Skip files that don't match the regex
 		}
 		fStat, err := os.Stat(path)
 		if err != nil {
@@ -110,15 +185,16 @@ func notifyChanFilesUpdated(lastModTime time.Time, dir string, ch chan time.Time
 		modTime := fStat.ModTime()
 		if modTime.After(lastModTime) {
 			fmt.Println(path, " Was saved, running command")
-			ch <- modTime
+			isModded = true
+			return nil
 		}
 		return nil
 	})
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return false, err
 	}
-	return nil
+	return isModded, nil
 }
 
 func getAbsolutePath(path string) (string, error) {
